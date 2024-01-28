ecb3abcb81572f5492e65376118c5cca53816c8a
Debesh Dasgupta <debesh.dasgupta@gmail.com>
2024-01-28 21:35:56 +0000

    First Version of StringCalculator

---
+++ src/AddNumbers.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AddNumbers {
+
+private int totalValue;
+private int acceptInput;
+
+    public String getRawinput() {
+        return Rawinput;
+    }
+
+    public void setRawinput(String rawinput) {
+        Rawinput = rawinput;
+    }
+
+    private String Rawinput;
+
+    public int getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(int totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public int getAcceptInput() {
+        return acceptInput;
+    }
+
+    public void setAcceptInput(int acceptInput) {
+        this.acceptInput = acceptInput;
+    }
+
+public int  AddValue() {
+      return totalValue + acceptInput ;
+}
+
+public void Checknulldata() {
+    System.out.println("Enetring checking if data changed");
+    if (Rawinput.isEmpty() ) {
+         this.setAcceptInput(0);
+    }else
+    {
+        this.setAcceptInput(Integer.valueOf(Rawinput));
+    }
+}
+
+
+    public static void main(String[] args) throws IOException {
+
+        int i = 0 ;
+        AddNumbers addNumbers = new AddNumbers();
+        int totfn = 0;
+        for (int j = 0 ; j<10 ; j++) {
+            System.out.println("Enter Number");
+            InputStreamReader inp = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(inp);
+            String inputValue = br.readLine();
+            addNumbers.setRawinput(inputValue);
+            addNumbers.Checknulldata();
+            System.out.println(addNumbers.getAcceptInput());
+
+
+            totfn = addNumbers.AddValue();
+            addNumbers.setTotalValue(totfn);
+            System.out.println("Hello world!");
+            System.out.println(addNumbers.getTotalValue());
+        }
+    }
+}
